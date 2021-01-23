0690fa19a9cc7499214c67eb3887e2f5bfa92bd7
SamokhinDmitro <samokhin.dmitriy.dev@gmail.com>
2021-01-23 19:35:43 +0200

    Main script file

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    /*Scroll to Top*/
+    new Scroll('.button_up');
+    /*End Scroll to Top*/
+
+    /*Next Section*/
+    new Next('.header-content__down');
+    /*END Next Section*/
+
+    /*Navigation*/
+    new Navigation('.header-navigation__btn', '.header-navigation', '.header-navigation__close');
+    /*END Navigation*/
+
+
+    /*Process Form Обработка форм данных от клиентов*/
+
+    //Обработка всех форм с data-form="client"
+    let allClientForms = document.querySelectorAll('form[data-form=client]');
+
+    allClientForms.forEach((el) => {
+        new SendForm().processForm(el);
+    });
+
+    /*END Process Form*/
+
+    //Заглушка для формы
+    const form = document.querySelector('.order-form');
+
+    form.addEventListener('submit', event => {
+    	event.preventDefault();
+    	alert('Демо версия сайта!');
+    	form.reset();
+    });
+
+    /*Preloader News*/
+    new News('.news');
+    /*End Preloader News*/
+});
